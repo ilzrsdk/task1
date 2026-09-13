a881f3d271cfc5e92016ebb015c5e3a10fb19d8f
ilzrsdk <hisokabyakuyavich@gmail.com>
2026-09-13 18:42:05 +0500

    Commit

---
+++ src/Main.kt
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    print("Введите текст: ")
+    val str = readln()
+    var str2 = ""
+    var count = 1
+    for (i in 0 until str.length - 1) {
+        if (str[i] == str[i+1]) {
+            count++
+        }else {
+            str2 += str[i]
+            if (count >= 2)
+            {
+                str2 += count
+            }
+            count = 1
+        }
+    }
+    str2 += str[str.length-1]
+    if (count >= 2)
+    {str2 += count}
+    println(str2)
+
+}
